776a73507baf8c476e9e9917412d75d597c11841
Olya <olkabony@ya.ru>
2016-07-15 11:37:02 +0300

    тестовые джейсоны

--- button.js
+++ button.js
@@ -6,14 +6,7 @@ center: [55.998333, -2.522778],
 zoom: 10,
 controls: []
 });
-var myPlacemark = new ymaps.Placemark([55.8, 37.6]);
-myMap.geoObjects.add(myPlacemark);
 
-var myCircle = new ymaps.Circle([[55.76, 37.64], 10000], 
-{
-    draggable: true
-});
-myMap.geoObjects.add(myCircle);
 
 var myButton =
 new ymaps.control.Button(
@@ -53,15 +46,7 @@ new ymaps.control.ListBoxItem('Нью-Йорк')
 ]
 });
 myMap.controls.add(myListBox);
-var mylacemark = new ymaps.Placemark([55.76, 37.64], {
-            // Чтобы балун и хинт открывались на метке, необходимо задать ей определенные свойства.
-            balloonContentHeader: "содержимое",
-            balloonContentBody: "Содержимое <em>балуна</em> метки",
-            balloonContentFooter: "Подвал",
-            hintContent: "Хинт метки"
-        });
 
-    myMap.geoObjects.add(mylacemark);
 
 var objectManager = new ymaps.LoadingObjectManager(
 "geoobjects.json",
@@ -98,6 +83,37 @@ paddingTemplate: 'myCallback',
 
 objectManager2.objects.options.set('preset', 'islands#greenDotIcon');
 objectManager2.clusters.options.set('preset', 'islands#greenClusterIcons');
-myMap.geoObjects.add(objectManager2);
+//myMap.geoObjects.add(objectManager2);
+ var obj = new ymaps.ObjectManager( {clusterize: true, gridSize: 125})
+myMap.geoObjects.add(obj);
+ 
+ $.ajax({
+        url: "dan\\test_0.json",
+		dataType: 'json',
+    }).done(function(data) {
+
+		new_data = { 
+			"type": "FeatureCollection",
+			"features": []
+		}
+		console.log(new_data);
+     	console.log (data)
+		for (var key in data){
+			feat = {
+				"type":"data[type]",
+				"id":14,
+				"geometry":{
+					"type": "Point", "coordinates": [data["coord"]["lat"], data["coord"]["lng"]]},
+					"properties": {"balloonContentBody": "Битвы", "balloonContentFooter": "Подвал",
+					"balloonContentHeader": "содержимое", "hintContent": "Битва при Данбаре 2"}
+			};
+			}
+		new_data = { 
+			"type": "FeatureCollection",
+			"features": [feat]
+			}
+		console.log(new_data);
+		obj.add(new_data);
+        });
 
 }
